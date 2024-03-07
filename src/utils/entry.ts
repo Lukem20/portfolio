5981adfff497dae44c563ffcbb49f7d3a2a22143
Luke Moore <lukemoore237@gmail.com>
2024-03-07 12:35:10 -0800

    Changed project structure for Three.js entry point.

---
+++ src/utils/entry.ts
@@ -0,0 +1,9 @@
+import { World } from '../components/World/World.js';
+
+async function main() {
+    const photoWheelContainer = document.querySelector('#scene-container');
+    const photoWheel = new World(photoWheelContainer);
+    photoWheel.start();
+}
+
+main();
