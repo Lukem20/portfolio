8fdc427f0d4a06c905f8def8d6ca38981799a1fb
Luke Moore <lukemoore237@gmail.com>
2024-03-07 15:43:21 -0800

    Loading images asynchronously.

--- src/components/World/systems/createTexture.js
+++ src/components/World/systems/createTexture.js
@@ -5,7 +5,7 @@ import {
 
 async function createTexture (imagePath) {
     const textureLoader = new TextureLoader();
-    const portfolioScreenshotImage = textureLoader.loadAsync(imagePath);
+    const portfolioScreenshotImage = await textureLoader.loadAsync(imagePath);
     
     return new MeshStandardMaterial({ 
         map: portfolioScreenshotImage,
