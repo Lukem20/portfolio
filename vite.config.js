4a6c12f0e66b22f906994febc3c0f1631f11ec21
Luke Moore <lukemoore237@gmail.com>
2024-06-30 15:50:29 -0700

    Updated config files with new project pages.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,15 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(root, 'index.html'),
-                about: resolve(root, 'about.html')
+                about: resolve(root, 'about.html'),
+                sevendeadlysins: resolve(root, '/projects/7ds.html'),
+                abts: resolve(root, '/projects/abts.html'),
+                brandidentitytimeline: resolve(root, '/projects/bit.html'),
+                graphicstextbook: resolve(root, '/projects/graphics-textbook.html'),
+                healdsburgcrush: resolve(root, '/projects/hbc.html'),
+                sandbox: resolve(root, '/projects/sandbox.html'),
+                sisisbarbershop: resolve(root, '/projects/sb.html'),
+                whatdesignerareyou: resolve(root, '/projects/wdru.html'),
             }
         },
         outDir,
