e2410edadd26c1c0e3e05056d776b5c361f26152
Luke <lukemoore237@gmail.com>
2024-08-30 20:45:27 -0700

    Added vite plugin to serve static JS file.

---
+++ vite-plugin-serve-static.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+
+export default function serveStaticPlugin() {
+  return {
+    name: 'vite-plugin-serve-static',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url.startsWith('/swiper-slides/')) {
+          const filePath = path.join(process.cwd(), 'src', req.url);
+          if (fs.existsSync(filePath)) {
+            res.setHeader('Content-Type', 'application/javascript');
+            fs.createReadStream(filePath).pipe(res);
+            return;
+          }
+        }
+        next();
+      });
+    }
+  };
+}
