c220645b04edc8e52bf1cccae9b87b0f2490d1d2
Moore, Luke P CTR (STF) USA <luke.moore@dliflc.edu>
2025-09-18 14:35:36 -0700

    Adjusted lights

--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -9,12 +9,13 @@ function createLights(scene) {
     const aLight = new AmbientLight(0xffffff, 0.1);
     aLight.tick = () => {};
 
-    const spotLight1 = createSpotlight( 0xFF7F00, 0, scene );
-    const spotLight2 = createSpotlight( 0x00FF7F, 120, scene );
-    const spotLight3 = createSpotlight( 0x7F00FF, 240, scene );
+    const spotLight1 = createSpotlight( 0xBFBFBF, 0, scene );
+    const spotLight2 = createSpotlight( 0xBFBFBF, 120, scene );
+    const spotLight3 = createSpotlight( 0xBFBFBF, 240, scene );
 
     const lights = new Group();
     lights.add( aLight, spotLight1, spotLight2, spotLight3 );
+    lights.position.y -= 90;
 
     return lights;
 }
@@ -22,10 +23,10 @@ function createLights(scene) {
 function createSpotlight(color, angleOffset, scene) {
     const light = new SpotLight(color, 30);
     light.castShadow = true;
-    light.angle = 0.45;
-    light.penumbra = 0.6;
-    light.decay = 0.54;
-    light.distance = 3000;
+    light.angle = 0.35;
+    light.penumbra = 0.8;
+    light.decay = 0.6;
+    light.distance = 600;
     light.shadow.mapSize.width = 1024;
     light.shadow.mapSize.height = 1024;
     light.shadow.camera.near = 0.1;
